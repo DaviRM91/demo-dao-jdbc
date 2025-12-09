4e31946fa22df61b265296ad38114d2b94bf4a53
DaviRM1 <davi.rm@outlook.com>
2025-12-09 16:18:10 -0300

    DepartmentDao e SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import model.entities.Department;
-import model.entities.Seller;
 
 import java.util.Date;
 
@@ -9,10 +8,7 @@ public class Program {
     public static void main(String[] args) {
 
 
-        Department dep = new Department(1, "Books");
-        Seller seller = new Seller(1, "Bob Brown", "bob@gmail.com", new Date(), 3000.00, dep);
 
-        System.out.println(seller);
 
     }
 }
