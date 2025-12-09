68c659aaa63a46d27e7d616fa0d5f93fa8d4986d
DaviRM1 <davi.rm@outlook.com>
2025-12-09 16:05:43 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,18 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
 public class Program {
+    public static void main(String[] args) {
+
+
+        Department dep = new Department(1, "Books");
+        Seller seller = new Seller(1, "Bob Brown", "bob@gmail.com", new Date(), 3000.00, dep);
+
+        System.out.println(seller);
+
+    }
 }
