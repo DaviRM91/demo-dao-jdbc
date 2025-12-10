1d5cf5a3c923e879df58151eeeaa62119fa21826
DaviRM1 <davi.rm@outlook.com>
2025-12-10 14:23:47 -0300

    find by Id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
 
     }
 }
