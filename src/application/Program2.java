0836cbbaa6c202e409003aa8536f412d343aed55
DaviRM1 <davi.rm@outlook.com>
2025-12-11 12:11:40 -0300

    Insert department

---
+++ src/application/Program2.java
@@ -0,0 +1,20 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Department newDepartment = new Department(null,"Jogos e Games");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New Department = " + newDepartment.getName());
+
+    }
+
+}
